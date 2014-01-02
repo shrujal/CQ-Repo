27f4a3fb0cbe82599428719f17f58731b6f4f79f
SP <shrupatel6@gmail.com>
2014-01-02 18:30:38 -0500

    first checkin

---
+++ spcqProject/bundle/src/main/java/com/sp/cq/spcqProject/HelloService.java
@@ -0,0 +1,13 @@
+package com.sp.cq.spcqProject;
+
+/**
+ * A simple service interface
+ */
+public interface HelloService {
+    
+    /**
+     * @return the name of the underlying JCR repository implementation
+     */
+    public String getRepositoryName();
+
+}
---
+++ spcqProject/bundle/src/main/java/com/sp/cq/spcqProject/package-info.java
@@ -0,0 +1,4 @@
+@Version("1.0.0")
+package com.sp.cq.spcqProject;
+
+import aQute.bnd.annotation.Version;
